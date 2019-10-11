10daa7ad76ae5e33e0ec146c05bf8bc1e682a757
Julia Luz Maciel dos Santos <julia.15081@gmail.com>
2019-10-10 22:35:33 -0300

    adding api structure

---
+++ api/hackathon2019/src/main/java/com/reggaeton/hackathon2019/Hackathon2019Application.java
@@ -0,0 +1,13 @@
+package com.reggaeton.hackathon2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon2019Application.class, args);
+	}
+
+}
---
+++ api/hackathon2019/src/test/java/com/reggaeton/hackathon2019/Hackathon2019ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reggaeton.hackathon2019;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hackathon2019ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
