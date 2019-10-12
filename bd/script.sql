02a9bb27b65b6b261f9471ebbe2958999c1858a6
Julia Luz Maciel dos Santos <julia.15081@gmail.com>
2019-10-12 01:19:44 -0300

    adicionando script do banco

--- bd/script.sql
+++ bd/script.sql
@@ -1,1 +1,10 @@
--- script
+create database hackathon2019;
+use hackathon2019;
+
+create table usuario (
+idusuario bigint auto_increment not null primary key,
+nome varchar(200) not null,
+datadenascimento date not null,
+matricula varchar(10) not null,
+email varchar(100) not null
+);
