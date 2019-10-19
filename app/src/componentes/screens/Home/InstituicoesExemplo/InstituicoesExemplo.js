aa4cd37e9ea3d6d59561ad5e4919660dcf28c0a6
Julia Luz Maciel dos Santos <julia.15081@gmail.com>
2019-10-19 12:08:12 -0300

    ident fix

--- app/src/componentes/screens/Home/InstituicoesExemplo/InstituicoesExemplo.js
+++ app/src/componentes/screens/Home/InstituicoesExemplo/InstituicoesExemplo.js
@@ -8,43 +8,42 @@ export class InstituicoesExemplo extends Component {
                 <h2 id="instituicoes-exemplo-title">Confira algumas de nossas Instituições Cadastradas.</h2>
                 <hr id="hr" />
                 <div id="instituicoes-exemplo-galeria">
-                        
-                        <div class="instituicoes-exemplo card">
-                            <img class="card-img-top" src="./img/4.png" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Instituição 01</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
-                                <button type="button" class="btn btn-warning">Saiba Mais</button>                                
-                            </div>
-                        </div>
-                        
-                        <div class="instituicoes-exemplo card ">
-                            <img class="card-img-top" src="..." alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Instituição 02</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
-                                <button type="button" class="btn btn-warning">Saiba Mais</button> 
-                            </div>
-                        </div>  
 
-                        <div class="instituicoes-exemplo card">
-                            <img class="card-img-top" src="..." alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Instituição 03</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
-                                <button type="button" class="btn btn-warning">Saiba Mais</button> 
-                            </div>
+                    <div class="instituicoes-exemplo card">
+                        <img class="card-img-top" src="./img/4.png" alt="Card cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Instituição 01</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
+                            <button type="button" class="btn btn-warning">Saiba Mais</button>
                         </div>
-                        
-                        <div class="instituicoes-exemplo card ">
-                            <img class="card-img-top" src="..." alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Instituição 04</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
-                                <button type="button" class="btn btn-warning">Saiba Mais</button> 
-                            </div>
-                        </div>  
-                       
+                    </div>
+
+                    <div class="instituicoes-exemplo card ">
+                        <img class="card-img-top" src="..." alt="Card cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Instituição 02</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
+                            <button type="button" class="btn btn-warning">Saiba Mais</button>
+                        </div>
+                    </div>
+
+                    <div class="instituicoes-exemplo card">
+                        <img class="card-img-top" src="..." alt="Card cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Instituição 03</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
+                            <button type="button" class="btn btn-warning">Saiba Mais</button>
+                        </div>
+                    </div>
+
+                    <div class="instituicoes-exemplo card ">
+                        <img class="card-img-top" src="..." alt="Card cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Instituição 04</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum eros eu augue porta hendrerit. Etiam maximus finibus suscipit.</p>
+                            <button type="button" class="btn btn-warning">Saiba Mais</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
